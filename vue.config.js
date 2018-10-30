8feba7d302bc6e8d7fc7409c5e5f369da85d9a6a
junfu <jeffy.junfu@gmail.com>
2018-10-30 19:55:26 +0800

    bugfix

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,8 @@ module.exports = {
   // https://www.foobar.com/my-app/
   // then change this to '/my-app/'
   baseUrl: BASE_URL,
+  parallel: require('os').cpus().length > 1,
+  runtimeCompiler: undefined,
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
@@ -33,6 +35,7 @@ module.exports = {
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
   },
+
   // 打包时不生成.map文件
   productionSourceMap: true
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
